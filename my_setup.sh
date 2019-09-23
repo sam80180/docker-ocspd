aa9d2ecd961ce5458a7128257c8b9413b772a50f
sam80180 <sam80180@gmail.com>
2019-09-23 18:31:57 +0800

    arreglo de las configuraciones de supervisor.

--- my_setup.sh
+++ my_setup.sh
@@ -6,5 +6,8 @@ echo "Asia/Taipei" > /etc/timezone
 rm -f /etc/localtime && ln -s /usr/share/zoneinfo/Asia/Taipei /etc/localtime
 dpkg-reconfigure -f noninteractive tzdata
 
+sed -i "s/\[supervisord\]/\[supervisord\]\nnodaemon=true\n/" /etc/supervisor/supervisord.conf
+
 # clean
 rm -f $0 ~/.bash_history ; exit
+
